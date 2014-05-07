926713d812394d402610e85a179d0e05beb20a39
Yondri Roa <yroa@upsidecorp.ch>
2014-05-07 02:34:54 -0430

    Fixing stuff

---
+++ src/com/cooltribes/migros/LoginActivity.java
@@ -0,0 +1,95 @@
+package com.cooltribes.migros;
+
+import java.util.HashMap;
+ 
+
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+ 
+import android.widget.Toast;
+import library.DatabaseHandler;
+import library.UserFunctions;
+ 
+public class LoginActivity extends Activity {
+    Button btnLogin;
+    Button btnLinkToRegister;
+    EditText inputEmail;
+    EditText inputPassword;
+    TextView loginErrorMsg;
+ 
+    // JSON Response node names
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
+    private static String KEY_ERROR_MSG = "error_msg";
+    private static String KEY_UID = "id";
+    private static String KEY_NAME = "username";
+    private static String KEY_EMAIL = "username";
+    private static String KEY_CREATED_AT = "createtime";
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+ 
+        // Importing all assets like buttons, text fields
+        inputEmail = (EditText) findViewById(R.id.loginEmail);
+        inputPassword = (EditText) findViewById(R.id.loginPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        loginErrorMsg = (TextView) findViewById(R.id.login_error);
+ 
+        // Login button Click Event
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                UserFunctions userFunction = new UserFunctions();
+                JSONObject json = userFunction.loginUser(email, password);
+                //Log.e("TAG", "Click:"+json);
+                // check for login response
+                try {
+                    if (json.getString(KEY_SUCCESS) != null) {
+                        loginErrorMsg.setText("");
+                        String res = json.getString(KEY_SUCCESS); 
+                        
+                        if(Integer.parseInt(res) == 1){
+                            // user successfully logged in
+                            // Store user details in SQLite Database
+                            DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                            JSONObject json_user = json.getJSONObject("user");
+                             
+                            // Clear all previous data in database
+                            userFunction.logoutUser(getApplicationContext());
+                            db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));                        
+                             
+                            // Launch Dashboard Screen
+                            Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+                             
+                            // Close all views before launching Dashboard
+                            dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(dashboard);
+                             
+                            // Close Login Screen
+                            finish();
+                        }else{
+                            // Error in login
+                            loginErrorMsg.setText("Incorrect username/password");
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ src/com/cooltribes/migros/MainActivity.java
@@ -0,0 +1,85 @@
+package com.cooltribes.migros;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+	private final static String TAG = "Debug";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button login_submit = (Button) findViewById(R.id.send);
+        login_submit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.e(TAG, "Click");
+				Toast.makeText(getApplicationContext(), "Submit click", Toast.LENGTH_SHORT).show();
+				
+				EditText username = (EditText) findViewById(R.id.username);
+				TextView label_username = (TextView) findViewById(R.id.label_username);
+				label_username.setText("Hola " + username.getText());
+			}
+		});
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
